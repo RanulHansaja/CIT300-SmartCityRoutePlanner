a470bf84363c505273ee40bb267148d74095f5bf
Y.C.ABEYSUNDARA <22ug3-0016@sltc.ac.lk>
2025-10-22 22:21:15 +0530

    Setup Smart City Route Planner main class structure

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        LocationTree tree = new LocationTree();
+        LocationManager manager = new LocationManager(graph, tree);
+        Scanner sc = new Scanner(System.in);
+        int choice;
+    
