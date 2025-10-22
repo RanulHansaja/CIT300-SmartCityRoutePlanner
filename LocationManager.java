1ac85496f3931da35a1c46a4ff3e3f1a2a76cfba
Ranul Hansaja <ranulh6@gmail.com>
2025-10-22 22:19:28 +0530

    Class setup and fields create

--- LocationManager.java
+++ LocationManager.java
@@ -1,1 +1,8 @@
+public class LocationManager {
+    private Graph graph;
+    private LocationTree tree;
 
+    public LocationManager(Graph graph, LocationTree tree) {
+        this.graph = graph;
+        this.tree = tree;
+    }
