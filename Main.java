c6d4bf75b9109000fb8a3bb8469a5b58914a727c
Y.C.ABEYSUNDARA <22ug3-0016@sltc.ac.lk>
2025-10-22 22:30:58 +0530

    Add main menu interface and user choice input handling

--- Main.java
+++ Main.java
@@ -7,4 +7,24 @@ public class Main {
         LocationManager manager = new LocationManager(graph, tree);
         Scanner sc = new Scanner(System.in);
         int choice;
-    
+
+        while (true) {
+            System.out.println("\n--- Smart City Route Planner ---");
+            System.out.println("1. Add a new location");
+            System.out.println("2. Remove a location");
+            System.out.println("3. Add a road between locations");
+            System.out.println("4. Remove a road");
+            System.out.println("5. Display all connections");
+            System.out.println("6. Display all locations");
+            System.out.println("7. Exit");
+            System.out.print("Enter your choice: ");
+
+            try {
+                choice = Integer.parseInt(sc.nextLine());
+            } catch (Exception e) {
+                System.out.println("Invalid input! Please enter a number from 1 to 7.");
+                continue; // redirect to main menu
+            }
+
+ 
+       
