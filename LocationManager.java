5822d06c55d98dbcaba66960eb08f81aeba46d9b
Ranul Hansaja <ranulh6@gmail.com>
2025-10-22 22:28:16 +0530

    Add location feature

--- LocationManager.java
+++ LocationManager.java
@@ -6,3 +6,12 @@ public class LocationManager {
         this.graph = graph;
         this.tree = tree;
     }
+
+      public void addLocation(String name) {
+        if (graph.addLocation(name)) {
+            tree.insert(name);
+            System.out.println("Location '" + name + "' added successfully.");
+        } else {
+            System.out.println("Location '" + name + "' already exists.");
+        }
+    }
