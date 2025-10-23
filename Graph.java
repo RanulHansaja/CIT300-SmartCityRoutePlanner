a601f325c704a223116ab606a2da15ee7962c402
sankalpi-2001 <sathyasankalpi2001@gmail.com>
2025-10-22 22:20:02 -0700

    Initialize Graph class with adjacency list structure

--- Graph.java
+++ Graph.java
@@ -1,1 +1,9 @@
+import java.util.*;
 
+public class Graph {
+    private Map<String, Set<String>> adjList = new HashMap<>();
+
+    private String normalize(String name) {
+        return name.trim().toLowerCase();
+    }
+}
